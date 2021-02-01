0897e49ae44edeb85ade04131a86a8365977e0ba
SMKrizan <smkrizan@gmail.com>
2021-01-31 20:35:10 -0600

    mutations added and functioning

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 // imports Apollo server
 const { ApolloServer } = require('apollo-server-express');
 
@@ -6,14 +7,19 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
 
+// imports middleware that ensures every request performs an authentication check, with updated request object passed to resolvers as the context
+const { authMiddleware } = require('./utils/auth');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // creates new Apollo server and passes in schema data so they know what our API looks like and how it resolves requests (there are more parameters that can be passed in, but the following two are required to get started
 // when Apollo server is connected to Express.js server a /graphql endpoint will be created for Express.js server to serve as main endpoint for accessing the entire API
+// 'context' method is set to return what will be made available in the resolvers; on the resolvers side, these headers become the 'context' parameter
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express applications as middleware
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-const secret = process.env.SECRET;
+const secret = `${process.env.SECRET}`;
 const expiration = '2h';
 
 module.exports = {
@@ -10,5 +10,34 @@ module.exports = {
         const payload = { username, email, _id };
 
         return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+
+    authMiddleware: function({ req }) {
+        // enables token to be sent via req.body, req.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // separate 'Bearer' from '<tokenvalue>'
+        if (req.headers.authorization) {
+            token = token
+            .split(' ')
+            .pop()
+            .trim();
+        }
+
+        // if no token, return request object as-is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // decode and attach user data to request object
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
+
+        // return updated request object
+        return req;
     }
 };
