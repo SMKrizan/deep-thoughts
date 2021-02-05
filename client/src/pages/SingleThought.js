820eafac1a2709f956882e13b611585d95a4f0b5
SMKrizan <smkrizan@gmail.com>
2021-02-04 19:37:42 -0600

    reaction functionality added

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -7,6 +7,8 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 // imports 'ReactionList' component
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
  
@@ -40,6 +42,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
